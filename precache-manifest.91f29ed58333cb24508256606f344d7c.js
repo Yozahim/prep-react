03a441d5bf99d8e5a0aaa5e2918dd069e440d08d
Sobaniec <konrad.sobaniec@capgemini.com>
2020-07-07 15:03:56 +0200

    Updates

---
+++ precache-manifest.91f29ed58333cb24508256606f344d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1836eb72fac83767a2a5cd56587ad218",
+    "url": "/prep-react/index.html"
+  },
+  {
+    "revision": "a4de58429457b1ec4864",
+    "url": "/prep-react/static/css/main.527badee.chunk.css"
+  },
+  {
+    "revision": "e329e118910816dcc073",
+    "url": "/prep-react/static/js/2.a42514e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prep-react/static/js/2.a42514e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4de58429457b1ec4864",
+    "url": "/prep-react/static/js/main.52784ed1.chunk.js"
+  },
+  {
+    "revision": "5e18a67f37c419dbfe97",
+    "url": "/prep-react/static/js/runtime-main.b436558c.js"
+  }
+]);
